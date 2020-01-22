844b0d2e1aacc3868988b7ef55d087ff2a6fb76b
root <root@sandbox.lan>
2020-01-22 06:38:21 +0000

    Finished Assignment 1 for Lab 1

--- 1/myprintf.c
+++ 1/myprintf.c
@@ -1,4 +1,4 @@
-// #include <stdio.h>
+#include <stdio.h>
 
 typedef unsigned int u32;
 
@@ -39,7 +39,8 @@ int printc(char x) {
 }
 
 int prints(char *x) {
-    for(int i = 0; x[i] != '\0'; i++) { printc(x[i]); }
+    for(int i = 0; x[i] != '\0'; i++)
+        printc(x[i]);
 }
 
 int printu(u32 x) {
@@ -47,62 +48,25 @@ int printu(u32 x) {
     putchar(' ');
 }
 
-int printd(int x) { //which prints an integer (x may be negative!!!)
-    if (x >= 0) {
-        putchar(x);
-    } else {
+int printd(int x) {
+    if (x < 0)
         putchar('-');
-        rpu(x * -1);
-    }
+    rpu(x);    
 }
 
-int printo(u32 x) { //which prints x in Octal (start with 0)
+int printo(u32 x) {
     putchar('0');
     (x == 0) ? putchar('0') : rpo(x);
 }
 
-int printx(u32 x) { //which prints x in HEX   (start with 0x)
+int printx(u32 x) {
     putchar('0');
     putchar('x');
-    putchar('0');
     
     (x == 0) ? putchar('0') : rph(x);
 }
 
 int myprintf(char *fmt, ...) {
-/*
-    Assume the call is myprintf(fmt, a,b,c,d);
-    Upon entry, the following diagram shows the stack contents.
-    
-                    char *cp -> "...%c ..%s ..%u .. %d\n"
-    HIGH               |                                              LOW 
-    --------------------------- --|------------------------------------------
-    | d | c | b | a | fmt |retPC| ebp | locals
-    ----------------|----------------|---------------------------------------
-                    |                | 
-                int *ip            CPU.ebp
-    
-        1. Let char *cp point at the format string
-
-        2. Let int *ip  point at the first item to be printed on stack:
-
-    *************** ALGORITHM ****************
-    Use cp to scan the format string:
-        spit out each char that's NOT %
-        for each \n, spit out an extra \r
-
-    Upon seeing a %: get next char, which must be one of 'c','s','u','d', 'o','x'
-    Then call YOUR
-
-            putchar(*ip) for 'c';
-            prints(*ip)  for 's';
-            printu(*ip)  for 'u';
-            printd(*ip)  for 'd';
-            printo(*ip)  for 'o';
-            printx(*ip)  for 'x';
-
-    Advance ip to point to the next item on stack.  */
-
     char x;
     int *ip = (int *) &fmt;
     for (int i = 0; fmt[i] != '\0'; i++) {        
@@ -141,9 +105,7 @@ int myprintf(char *fmt, ...) {
 }
 
 int main(int argc, char *argv[], char *env[]) {
-    myprintf("argc:\t%d\n", argc);
-    for(int i = 0; i < argc; i++) {
-        myprintf("argv[%d]:\t%s\n", i, argv[i]);
-    }
-    myprintf("cha=%c string=%s      dec=%d hex=%x oct=%o neg=%d\n", 'A', "this is a test", 100, 100, 100, -100);
+    myprintf("argc:\t\t%d\n", argc);
+    for(int i = 0; i < argc; i++)
+        myprintf("argv[%d]: \t%s\n", i, argv[i]);
 }
