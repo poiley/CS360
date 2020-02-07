c18a9a3e5b764a57af8d50a2e633b7aae1f42016
bpoile <bpoile@eecs.wsu.esu>
2020-02-06 18:04:15 -0800

    comments

--- 2/sh.c
+++ 2/sh.c
@@ -40,78 +40,77 @@ void tok(char src[]) {
 
 /* Change the directory that the program is pointing to. By deault, Home. */
 int cd() {
-    if (strcmp(input[1], "\0") == 0)
+    if (strcmp(input[1], "\0") == 0)    // If no directory specified, go home.
         return chdir(getenv("HOME"));
-    return chdir(input[1]); 
+    return chdir(input[1]);             // Go to directory specified.
 }
 
 /* Redirection Handler. Opens the file with applicable permissions. */
-int redir(int direction, char file[]) {
-    switch(direction) {
-        case 1:
+int redir(int o, char file[]) {
+    switch(o) {                         // int o determines file open configuration. 
+        case 1:                         // case 1: <    Open file as Read Only.
             close(0);
-            return open(file, O_RDONLY);
-        case 2:
+            return open(file, O_RDONLY);//open file as read only
+        case 2:                         // case 2: >    Open or create file as Write Only.
             close(1);
             return open(file, O_WRONLY | O_CREAT, 0644);
-        case 3:
+        case 3:                         // case 3: >>   Open file as Write Only and append.
             close(1);
-            return open(file, O_WRONLY | O_APPEND, 0644);
-        default:
-            return 1;
+            return open(file, O_WRONLY | O_APPEND, 0644);    
     }
+    return 1; // Default: Edge case, no o
 }
 
 /* Redirection Specifier. Iterates through Argv, looks for indirection cases, and calls the Redirection Handler accordingly. */
 int isRedir(char *argv[]) {
     char temp[16];
-    for(int i = 0; argv[i]; strncpy(temp, argv[i++], 16))
-        if (strcmp(temp, "<") == 0) { 
-            strncpy(temp, argv[i], 16);
+    for(int i = 0; argv[i]; strncpy(temp, argv[i++], 16)) // If <, >, or >>, copy the argument string each time and redirect.
+        if (strcmp(temp, "<") == 0) {           // case 1: <    Copy string, redirect.
+            strncpy(temp, argv[i], 16); 
             return redir(1, temp);
-        } else if (strcmp(temp, ">") == 0) { 
-            strncpy(temp, argv[i], 16);
+        } else if (strcmp(temp, ">") == 0) {    // case 2: >    Copy string, redirect. 
+            strncpy(temp, argv[i], 16);     
             return redir(2, temp);
-        } else if (strcmp(temp, ">>") == 0) { 
+        } else if (strcmp(temp, ">>") == 0) {   // case 3: >>   Copy string, redirect.
             strncpy(temp, argv[i], 16);
             return redir(3, temp);
         }
-    return -1;
+    return -1;                                  // case default. return -1.
 }
 
 /* Execution Handler. Handles and executes any command in the PATH, which for now is just /bin/ */
 int exec(char *argv[]){
-    char path[16] = "/bin/";
+    char path[16] = "/bin/";            // PATH: Eventually will be an environment variable, but for now it just refers to /bin/
     strcat(path, argv[0]);  
 
-    if (isRedir(argv) != -1) 
+    if (isRedir(argv) != -1)            // If redirect, set data.
         argv[ARGC - 2] = NULL;
 
-    if (execve(path, argv, NULL) == -1) 
+    if (execve(path, argv, NULL) == -1) // Execute and handle errors.
         printf("%s\n", strerror(errno));
 }
 
 /* Pipe Handler: Pipes commands and combines data. */
 void concatPipe(char *head[], char *tail[]) {
     int pd[2], pid;
-    pipe(pd); 
+    pipe(pd);           // Determines and routes the input/output for piped processes.
 
-    pid = fork(); 
+    pid = fork();       // Fork.
 
-    if (pid) {        
-        close(pd[0]); 
+    if (pid) {
+        close(pd[0]);   // Close the input stream.
 
         close(1);     
         dup(pd[1]);   
         close(pd[1]); 
-        exec(head);   
+        exec(head);     // Execute.
     } else {          
-        close(pd[1]); 
+        close(pd[1]);   // Close the input stream.
 
         close(0);     
         dup(pd[0]);   
         close(pd[0]); 
-        exec(tail);   
+        exec(tail);     // Execute.
     }
 }
 
@@ -131,52 +130,58 @@ int isPipe(char *head[], char *tail[]) {
 
 /* Function to handle the forking of child processes. */
 int forkC() {
-    int pid = fork(), status;
+    int pid = fork(), status; // Fork child process, and create status int.
 
     if (pid > 0) {
-        printf("Parent %d is now waiting for child %d to die.\n", getpid(), pid);
+        printf("[LOG] Parent %d is now waiting for child %d to die.\n", getpid(), pid);
         
-        pid = wait(&status);
+        pid = wait(&status); // Wait for child process.
         
-        printf("Dead Child = %d, By = %04x\n", pid, status);
+        printf("[LOG] Dead Child = %d, By = %04x\n", pid, status);
     } else if (pid == 0) { 
-        char *head[16], *tail[16];
+        char *head[16], *tail[16];      // Create Head and Tail.
 
-        if (isPipe(head, tail) == 1)
-            concatPipe(head, tail);
+        if (isPipe(head, tail) == 1)    // Check for pipes in Head and Tail.
+            concatPipe(head, tail);     // Concat Pipe data.
         else
             exec(ARGV);
         
-        exit(0);
+        exit(0);                        // Exit.
     } else  
-        printf("Unable to create process!\n");
+        printf("[LOG] Unable to create process!\n");    // Report Error.
+}
+
+/* Function that gets the CWD and returns it as a *char */
+char *cwd() {
+    char cwd[256], *cwdout = (char *) malloc(sizeof(cwd));
+    getcwd(cwd, sizeof(cwd));   // Get current working directory
+    strcpy(cwdout, cwd);        // Copy the string
+    return cwdout;              // Return
 }
 
 /* Execution Specifier. Takes the information in Argv and calls commands accordingly. */
 void execCMD(){
-    generateArgv(ARGV); 
+    generateArgv(ARGV);                      // Populate global Argv.
 
-    if (strcmp(input[0], "exit") == 0) 
-        exit(1);
-    else if (strcmp(input[0], "cd") == 0) {
-        if (cd() == 0) 
-            printf("cd to %s was a Success.\n", (strcmp(input[1], "\0") == 0) ? "$HOME" : input[1]);
+    if (strcmp(input[0], "cd") == 0) {       // Check if the user has entered cd, which is a special case.
+        if (cd() == 0)                       // Default: cd to Home directory if cd directory isn't specified.
+            printf("[LOG] cd to %s was a Success.\n", (strcmp(input[1], "\0") == 0) ? "$HOME" : input[1]);
         else
-            printf("cd to %s was a Failure.\n", input[1]);
+            printf("[LOG] cd to %s was a Failure.\n", input[1]);
 
-        char cwd[256];
-        getcwd(cwd, sizeof(cwd));
-        printf("CWD ~> %s\n", cwd);
-    } else
-        forkC();
+        printf("[LOG] CWD ~> %s\n", cwd());   // Output the current working directory.
+    } else if (strcmp(input[0], "exit") == 0) // Otherwise, if the has entered the exit command...
+        exit(1);                              // Stop the program.
+    else
+        forkC();                              // Execute the child command
 }
 
 /* Main function. Handles the running of the program in a loop */
 int main(int argc, char *argv[]) {
-    char tempLine[128];
+    char tempLine[128];             // Temporary string variable initialization.
 
     while (1) {                     // No exit condition, as program exit is handle in execCMD()
-        printf("$ ");               // PS1 Prompt
+        printf("%s$ ", cwd());      // Line Prompt
         fflush(stdout);             // Flush
 
         fgets(line, 128, stdin);    // Read user input, write to line
