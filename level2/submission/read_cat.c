eeb8d435d2d547e73e22641280632480826db7c9
lbaccus <lauren.baccus@wsu.edu>
2020-12-04 16:35:26 -0800

    Formatting

--- level2/submission/read_cat.c
+++ level2/submission/read_cat.c
@@ -1,6 +1,6 @@
 #include "../level1/commands.h"
 
-int read_file(char *fd, char *bytes){
+int read_file(char *fd, char *bytes) {
     int i_fd = atoi(fd), i_bytes = atoi(bytes);
     char buf[BLKSIZE];
     printf("[read_file]: Reading fd=%d\n", i_fd);
@@ -8,7 +8,7 @@ int read_file(char *fd, char *bytes){
     return myread(i_fd, buf, i_bytes, 0);
 }
 
-int min3(int a, int b, int c){
+int min3(int a, int b, int c) {
     if (a <= b && a <= c)
         return a;
     else if (b <= c && b <= a)
@@ -17,11 +17,11 @@ int min3(int a, int b, int c){
         return c;
 }
 
-int myread(int fd, char *buf, int nbytes, int supress_msg){
+int myread(int fd, char *buf, int nbytes, int supress_msg) {
     char readbuf[BLKSIZE];
     int min, count=0, blk, lblk, dblk, start, remain, avail, ibuf[256], dbuf[256];
 
-    if (running->fd[fd] == NULL){ // make sure fd exists
+    if (running->fd[fd] == NULL) { 
         printf("[myread]: fd is NULL!");
         return -1;
     }
@@ -35,24 +35,22 @@ int myread(int fd, char *buf, int nbytes, int supress_msg){
     if (!supress_msg)
         printf("[myread]: fd=%d offset=%d bytes=%d\n", fd, oftp->offset, nbytes);
 
-    while (nbytes && avail){ // read loop
+    while (nbytes && avail) {
         lblk = oftp->offset / BLKSIZE;
         start = oftp->offset % BLKSIZE;
 
-        if (lblk < 12){ // direct blocks
+        if (lblk < 12) {
             if (!supress_msg)
                 printf("[myread]: direct block\n");
             blk = mip->inode.i_block[lblk];
-        }
-        else if (lblk >= 12 && lblk < 256 + 12){ // indirect blocks
+        } else if (lblk >= 12 && lblk < 256 + 12) { 
             if (!supress_msg)
                 printf("[myread]: indirect block\n");
 
-            get_block(mip->dev, mip->inode.i_block[12], (char*)ibuf); // from book
+            get_block(mip->dev, mip->inode.i_block[12], (char*)ibuf); 
             blk = ibuf[lblk-12];
-        }
-        else{
-            if (!supress_msg)
+        } else {
+             if (!supress_msg)
                 printf("[myread]: double indirect block\n");
             
             lblk -= 268;
@@ -64,15 +62,13 @@ int myread(int fd, char *buf, int nbytes, int supress_msg){
         }
 
         get_block(mip->dev, blk, readbuf);
-        char *cp = readbuf + start; // takes care of offset
+        char *cp = readbuf + start; 
         remain = BLKSIZE - start;
 
-        // read optimization
         min = min3(nbytes, avail, remain);
         if (!supress_msg)
             printf("[myread]: offset=%d min=%d blk=%d\n", oftp->offset, min, blk);
         strncpy(buf, cp, min);
-        //char *cq = buf;
 
         oftp->offset += min;
         count += min;
@@ -80,37 +76,28 @@ int myread(int fd, char *buf, int nbytes, int supress_msg){
         nbytes -= min;
         remain -= min;
 
-        //while (remain > 0){
-        //    *cq++ = *cp++; // cpy byte into buf
-        //    oftp->offset++;
-        //    count++; // inc offset and count
-        //    avail--; nbytes--; remain--; // dec avail, nbytes and remain
-        //    if (nbytes <= 0 || avail <= 0)
-        //        break;
-        //}
-        if (!supress_msg){
+        if (!supress_msg) {
             printf("[myread]: nbytes=%d len(buf)=%d", count, (int)strlen(buf));
             printf(" text=%s\n", buf);
         }
     }
-
     return count;
 }
 
-int cat_file(char *filename){
+int cat_file(char *filename) {
     char mybuf[BLKSIZE];
     int len=0, n, i, fd = open_file(filename, "0");
 
     if (fd < 0) return -1;
 
-    mybuf[BLKSIZE]=0; // terminate mybuf
+    mybuf[BLKSIZE]=0;
 
     printf("[cat_file]:\n\n");
-    while ((n = myread(fd, mybuf, BLKSIZE, 1))){
+    while ((n = myread(fd, mybuf, BLKSIZE, 1))) {
         mybuf[n]=0;
 
-        for (i=0; i<n; i++){
-            if (mybuf[i] == '\\' && mybuf[i++] == 'n'){
+        for (i=0; i<n; i++) {
+            if (mybuf[i] == '\\' && mybuf[i++] == 'n') {
                 putchar('\n');
                 continue;
             }
--- level2/submission/write.c
+++ level2/submission/write.c
@@ -1,7 +1,6 @@
 #include "../level1/commands.h"
 
-int write_file()
-{
+int write_file() {
     char string[BLKSIZE];
     int fd;
     pfd();
@@ -19,8 +18,7 @@ int write_file()
     return -1;
 }
 
-int mywrite(int fd, char buf[], int nbytes)
-{
+int mywrite(int fd, char buf[], int nbytes) {
     printf("[mywrite]: %d %d\n", (int)strlen(buf), nbytes);
     int count = nbytes, blk, dblk;
     int ibuf[256], dbuf[256], buf13[256];
@@ -29,54 +27,29 @@ int mywrite(int fd, char buf[], int nbytes)
     oftp = running->fd[fd];
     MINODE *mip;
     mip = oftp->minodePtr;
-    while(nbytes > 0)
-    {
-        // Logical block
+    while(nbytes > 0) {
         int lbk = oftp->offset / BLKSIZE;
-        // Start Byte
         int startByte = oftp->offset % BLKSIZE;
-        // Direct Block
-        if(lbk <12)
-        {
+        if(lbk <12) {
             printf("[mywrite]: Direct Block\n");
-            // If no data block is present, allocate one
             if(mip->inode.i_block[lbk] == 0)
                 mip->inode.i_block[lbk] = balloc(mip->dev);
-            // Saves Block into blk
-            blk  = mip->inode.i_block[lbk];
-        }
-        // Indirect Block
-        else if(lbk >= 12 && lbk < 256 + 12)
-        {
+\            blk  = mip->inode.i_block[lbk];
+        } else if(lbk >= 12 && lbk < 256 + 12) {
             printf("[mywrite]: Indirect Block\n");
-	        // Like above, if no data block allocate one
-            if(mip->inode.i_block[12] == 0)
-            {
-                // Allocate block
+            if(mip->inode.i_block[12] == 0) {
                 mip->inode.i_block[12] = balloc(mip->dev);
-                // Get block into memmory
                 get_block(mip->dev, mip->inode.i_block[12], (char *)ibuf);
-                // Zero it out
                 bzero(ibuf, 256);
-                //memset(ibuf, 0, 256);
-                // Put block into memory
                 put_block(mip->dev,mip->inode.i_block[12], (char *)ibuf);
             }
-            // Get block into ibuf
             get_block(mip->dev, mip->inode.i_block[12], (char *)ibuf);
-            // Set blk to disk block
             blk = ibuf[lbk -12];
-            // If 0 allocate it
-            if(blk ==0)
-            {
+            if(blk ==0) {
                 mip->inode.i_block[12] = balloc(mip->dev);
                 ibuf[lbk -12] = mip->inode.i_block[lbk];
             }
-        }
-        // Double indirect blocks
-        else
-        {
-            // From the help posted by KC
+        } else {
             printf("[mywrite]: Double Indirect Block\n");
             lbk -= (256+12);
             get_block(mip->dev,mip->inode.i_block[13], (char *)buf13);
@@ -84,31 +57,22 @@ int mywrite(int fd, char buf[], int nbytes)
             get_block(mip->dev, dblk, (char *)dbuf);
             blk = dbuf[lbk %256];
         }
-        // Reset wbuf as to not add too many characters in the last buffer
         memset(wbuf, 0, BLKSIZE);
-        // Get data block blk into wbuf
         get_block(mip->dev, blk, wbuf);
-        // Set the start point of wbuf
         char *cp = wbuf + startByte;
         int remain = BLKSIZE -startByte;
         char *cq = buf; 
-        // Write until remain == 0
-        while(remain > 0)
-        {
+        while(remain > 0) {
             *cp++ = *cq++;
             nbytes--; remain--;
             oftp->offset++;
-            // If offset is greater than size increase file size
             if(oftp->offset > mip->inode.i_size)
                 mip->inode.i_size++;
-            // Break when you have read all the bytes
             if(nbytes <= 0)
                 break;
         }
-        // Put wbuf into data block blk
         put_block(mip->dev,blk,wbuf);
     }
-    // Mark as dirty to save when done
     mip->dirty =1;
     printf("[mywrite]: Wrote %d char into file descriptor fd=%d\n", count,fd);
     return nbytes;
